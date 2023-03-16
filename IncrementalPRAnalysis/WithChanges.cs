79beae77ca683cdf9227e601ac17ffed8d0a1883
Costin Zaharia <costin.zaharia@sonarsource.com>
2023-03-16 10:55:56 +0100

    Delete some code

--- IncrementalPRAnalysis/WithChanges.cs
+++ IncrementalPRAnalysis/WithChanges.cs
@@ -5,19 +5,4 @@ public class WithChanges
     private void UnusedMethod()
     {
     }
-
-    public int AnotherUnusedMethod(bool a, bool b)
-    {
-        string unusedVar = null;
-
-        if (a)
-        {
-            if (b)
-            {
-                return 0;
-            }
-        }
-
-        return 1;
-    }
 }
